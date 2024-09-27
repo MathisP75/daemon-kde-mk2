fbd33cbb6fe461ab4a5d5454a77d4243e5d4937f
MathisP75 <mathispaquet362@gmail.com>
2024-09-27 12:06:11 -0400

    Changed icon theme and Plasma Style

--- Daemon-Icons/scalable/svg-color-change.sh
+++ Daemon-Icons/scalable/svg-color-change.sh
@@ -5,7 +5,7 @@ do
     if [ ! -h $f ] 
         then
             echo "Changing color of $f ..."
-            sed -i -e 's/#e6e6e6/#5DF4FE/g' "$f";
+            sed -i -e 's/#....../#5DF4FE/g' "$f";
     fi
 done
 
